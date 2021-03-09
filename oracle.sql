f535e51db208544c55a1d7c8621a039e0019b31e
notemac <43244677+notemac@users.noreply.github.com>
2021-03-09 16:41:12 +0300

    Update oracle.sql

--- oracle.sql
+++ oracle.sql
@@ -41,3 +41,51 @@ from
     dual 
 connect 
     by level <= MONTHS_BETWEEN(LAST_DAY(TO_DATE('@{pipeline().parameters.DateTo}', 'YYYY-MM-DD')), TO_DATE('@{pipeline().parameters.DateFrom}', 'YYYY-MM-DD')) + 1;
+                                                                                                           
+                                                                                                           
+with months as
+(
+    select 
+        case
+            when level = 1
+            then '@{pipeline().parameters.DateFrom}'
+            else TO_CHAR(TRUNC(ADD_MONTHS(TO_DATE('@{pipeline().parameters.DateFrom}', 'YYYY-MM-DD'), level - 1), 'MONTH'), 'YYYY-MM-DD')
+            end as DateFrom
+        ,  
+        case
+            when '@{pipeline().parameters.DateFrom}' = '@{pipeline().parameters.DateTo}'
+            then '@{pipeline().parameters.DateFrom}'
+            when level = TRUNC(MONTHS_BETWEEN(LAST_DAY(TO_DATE('@{pipeline().parameters.DateTo}', 'YYYY-MM-DD')), TO_DATE('@{pipeline().parameters.DateFrom}', 'YYYY-MM-DD'))) + 1
+            then '@{pipeline().parameters.DateTo}'
+            else TO_CHAR(ADD_MONTHS(LAST_DAY(TO_DATE('@{pipeline().parameters.DateFrom}', 'YYYY-MM-DD')), level - 1), 'YYYY-MM-DD')
+            end as DateTo
+    from 
+        dual 
+    connect 
+        by level <= MONTHS_BETWEEN(LAST_DAY(TO_DATE('@{pipeline().parameters.DateTo}', 'YYYY-MM-DD')), TO_DATE('@{pipeline().parameters.DateFrom}', 'YYYY-MM-DD')) + 1
+)
+select
+    p.DateFrom
+    , p.DateTo
+from
+    months m
+    cross apply (
+        select 
+            m.DateFrom
+            ,
+            case
+                when extract(day from to_date(m.DateFrom, 'YYYY-MM-DD')) <= 15 and extract(day from to_date(m.DateTo, 'YYYY-MM-DD')) > 15
+                then to_char(trunc(to_date(m.DateFrom, 'YYYY-MM-DD'), 'MONTH') + 14, 'YYYY-MM-DD') 
+                else m.DateTo
+            end as DateTo
+        from dual
+
+        union all
+
+        select 
+            to_char(trunc(to_date(m.DateFrom, 'YYYY-MM-DD'), 'MONTH') + 15, 'YYYY-MM-DD') as DateFrom
+            , m.DateTo
+        from dual
+        where
+            extract(day from to_date(m.DateFrom, 'YYYY-MM-DD')) <= 15 and extract(day from to_date(m.DateTo, 'YYYY-MM-DD')) > 15
+    ) p
